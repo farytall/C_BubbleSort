9fcb348fafb87445a8bc6fe7a31193288589d20c
farytall <k.n.nurmukhametov@gmail.com>
2021-09-30 11:01:49 +0300

    C_BubbleSort.cpp

--- C_BubbleSort/C_BubbleSort.cpp
+++ C_BubbleSort/C_BubbleSort.cpp
@@ -2,30 +2,36 @@
 //
 
 #include <iostream>
-
-int main()
-{
     const int n = 10;
-    int change, a[n], m = n - 1;
-    srand(time(0));
-    for (int i = 0; i < n; i++) {
-        a[i] = rand() % 1000;
-    }
-    for (int j = 0; j < m; j++) {
-        std::cout << " sort step*" << j << " - ";
-        for (int i = 0; i < n; i++) {
+    int a[n], change, i, j, t;
+    void randomize() {
+        srand(time(0));
+        for (i = 0; i < n; i++) {
+            a[i] = rand() % 100;
             std::cout << a[i] << " ";
         }
         std::cout << "\n";
-        for (int i = 0; i < m; i++) {
-            if (a[i] > a[i + 1]) {
-                change = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = change;
+    }
+    void bubble() {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    change = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = change;
+                }
             }
         }
+        for (t = 0; t < n; t++) {
+            std::cout << a[t] << " ";
+        }
     }
-    std::cout << "sort complete\n";
+int main()
+{
+   randomize();
+   bubble();
+   std::cout << "\n sort complete";
+   std::cin.get();
 }
 
 
