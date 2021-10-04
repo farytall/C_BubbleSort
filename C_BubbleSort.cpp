a9dc1ab46314f40fba2051ccc348ccbe35650e9a
farytall <91369234+farytall@users.noreply.github.com>
2021-10-04 21:27:05 +0300

    Add files via upload

---
+++ C_BubbleSort.cpp
@@ -0,0 +1,20 @@
+﻿// BubbleSoort1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "BubbleSort.h"
+#include <time.h>
+int main()
+{
+	srand(time(0));
+	BubbleSort var;
+	var.rand_element();
+	var.SortMethod();
+	var.show_result();
+	var.serch_Minimal();
+
+}
+
+
+
+
